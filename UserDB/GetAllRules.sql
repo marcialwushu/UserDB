c298fcc7094beece1b076ab4e12f0a872497efb9
Cleilson <marcialwushu@gmail.com>
2020-12-25 19:10:27 -0300

    Add Stored Procedure [GetAllRules]

---
+++ UserDB/GetAllRules.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetAllRules]
+	@paramName [nvarchar](256),
+	@paramNormalizedName [nvarchar](256)
+AS
+BEGIN
+	SELECT 
+		[Name]
+		[NormalizedName]
+	FROM
+		[dbo].[AspNetRoles] WITH (NOLOCK)
+	WHERE 
+		[Name] = @paramName AND
+		[NormalizedName] = @paramNormalizedName
+END
